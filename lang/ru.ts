98cb49af38455dc019e43e3d58f7a990d2a8f591
Mark <markingram.edu@outlook.com>
2026-02-20 02:15:57 +0200

    UI text correction

--- lang/ru.ts
+++ lang/ru.ts
@@ -1,20 +1,17 @@
 export default {
     MENU_TITLE: "Опубликовать в Telegram",
-    MENU_SUBMENU_TITLE: "Отправить в...",
     NOTICE_SUCCESS: "Успешно опубликовано ✅",
     NOTICE_ERR_CONFIG: "Ошибка: Настройте хотя бы один канал в параметрах.",
     NOTICE_ERR_SEND: "Ошибка отправки: ",
     SETTING_HEADER: "Настройки Publish to Telegram",
     SETTING_ADD_CHANNEL: "Добавить пресет публикации",
-    SETTING_CHANNEL_NAME: "Название пресета",
     SETTING_BOT_TOKEN_NAME: "Токен бота",
     SETTING_BOT_TOKEN_DESC: "Получите его у бота @BotFather",
     SETTING_CHAT_ID_NAME: "ID канала/группы",
     SETTING_CHAT_ID_DESC: "Получите его у бота @userinfobot",
     SETTING_DELETE_CHANNEL: "Удалить пресет",
     SETTING_DEFAULT_CHANNEL: "Установить как пресет по умолчанию",
-    SETTING_DEFAULT_DESC: "Можно вызвать из палитры команд или по сочетанию клавиш.",
-    SETTING_PLACE_NAME: "Например: Мой Блог",
+    SETTING_DEFAULT_DESC: "Пресет будет использоваться для публикации с помощью сочетания клавиш.",
     SETTING_PLACEHOLDER_TOKEN: "Введите токен...",
     SETTING_PLACEHOLDER_CHAT: "Введите ID..."
 };
